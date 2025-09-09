585c71df31fee65b35b34f00548de732504f0ff4
Anurag Raj <adityanv8@gmail.com>
2025-09-09 19:14:56 +0530

    importing navbar from packages to apps without creating new packages

---
+++ Monorepos/my-turbo/packages/ui/src/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul style={{display:"flex",justifyContent:"center",alignItems:"center",gap:'50px',background:"red",color:"white"}}>
+       <li>Home</li>
+       <li>About</li>
+       <li>Contact</li>
+       <li>Menu</li>
+      </ul>  
+    </div>
+  )
+}
+
+export default Navbar
