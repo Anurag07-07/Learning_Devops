db2fedb6fb1498ef827f79367e0ab2796d463dd3
Anurag Raj <adityanv8@gmail.com>
2025-09-10 23:55:13 +0530

    changes

--- Monorepos/my-turbo/apps/http-server/src/index.ts
+++ Monorepos/my-turbo/apps/http-server/src/index.ts
@@ -2,6 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.listen(3000,()=>{
-  console.log(`Server start at PORT 3000`);
+app.listen(3005,()=>{
+  console.log(`Server start at PORT 3005`);
 })
