e4817d18ac3bd9ef1ddd952a138bef60c53dd219
Anurag Raj <adityanv8@gmail.com>
2025-09-18 18:59:26 +0530

    Added React App

--- Monorepos/Readme.md
+++ Monorepos/Readme.md
@@ -20,3 +20,5 @@ Turborepo is not exactly a monorepo framework
 ![alt text](image-4.png)
 
 ![alt text](image-5.png)
+
+![alt text](image-6.png)
---
+++ Monorepos/my-turbo/apps/react-app/src/App.tsx
@@ -0,0 +1,10 @@
+import {Button} from '@repo/ui/button'
+const App = () => {
+  return (
+    <div>
+      <Button appName='react-app' >Signup Button</Button>
+    </div>
+  )
+}
+
+export default App
