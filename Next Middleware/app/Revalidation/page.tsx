d6c392352194d13e85dcc7030adaef94058147f8
Anurag Raj <adityanv8@gmail.com>
2025-09-23 18:53:12 +0530

    cache maintenence

--- Next Middleware/app/Revalidation/page.tsx
+++ Next Middleware/app/Revalidation/page.tsx
@@ -1,19 +1,25 @@
+import revalidate from "../lib/actions/action1"
+
 export default async function Page(){
   
-  const res = await fetch('http://sum-server.100xdevs.com/todos',{
-    next:{
-      revalidate:10 //After 10 sec the page will again rebuild    
-    }
-  })
+  // const res = await fetch('http://sum-server.100xdevs.com/todos',{
+  //   next:{
+  //     revalidate:10 //After 10 sec the page will again rebuild    
+  //   }
+  // })
 
-  const data = await res.json()
-  console.log(JSON.stringify(data));
+  // const data = await res.json()
+  // console.log(JSON.stringify(data));
+
+  const response = await fetch('https://sum-server.100xdevs.com/todos',{next:{tags:['todos']}}) //Endpoint Name
+  const data = await response.json()
+  revalidate() //On Every Reload the new todos that were added will be shown there
   return <div>
       {
-        data.map(({todos})=>(
+        data.map(({todo}:any)=>(
          <div key={Math.random()}>
-           {todos.title}
-           {todos.description}
+           {todo.title}
+           {todo.description}
          </div>
         ))
       }
