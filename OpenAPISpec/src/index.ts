1495b869b4b98a18a30aa33fe894995ec6a435d7
Anurag Raj <adityanv8@gmail.com>
2025-10-02 16:31:51 +0530

    open api spec completed

---
+++ OpenAPISpec/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import SwaggerUI from 'swagger-ui-express'
+import { openApiSpec } from './openapispec.js';
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use('/documentation',SwaggerUI.serve,SwaggerUI.setup(openApiSpec))
+
+let users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' }
+];
+
+app.get('/users', (req, res) => {
+    const { name } = req.query;
+
+    if (name) {
+      //@ts-ignore
+        const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowercase()));
+        res.json(filteredUsers);
+    } else {
+        res.json(users);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
