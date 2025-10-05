efb1e7ababa2e1aeab86be67b546d6cb872d4dfc
Anurag Raj <adityanv8@gmail.com>
2025-10-05 15:12:34 +0530

    backend Logic Done

---
+++ Rate Limiting/backend/dist/index.js
@@ -0,0 +1,35 @@
+import express, {} from 'express';
+const app = express();
+const otpStore = {};
+app.post('/generate-otp', (req, res) => {
+    const { email } = req.body;
+    if (!email) {
+        return res.status(400).json({ message: `Email is Required` });
+    }
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    otpStore[email] = otp;
+    console.log(`OTP for ${email}: ${otp}`);
+    res.status(200).json({
+        message: `OTP is Generated`
+    });
+});
+app.post('/reset-password', (req, res) => {
+    const { email, otp, newPassword } = req.body;
+    if (!email || !otp || !newPassword) {
+        return res.status(400).json({
+            message: `Email, OTP and new password are required`
+        });
+    }
+    if (otpStore[email] === otp) {
+        console.log(`Password for ${email} has been reset to :${newPassword}`);
+        delete otpStore[email];
+        res.status(200).json({ message: `Password has been reset Successfully` });
+    }
+    else {
+        res.status(401).json({ message: `Invalid OTP` });
+    }
+});
+app.listen(3000, () => {
+    console.log(`Server started at port  ${3000}`);
+});
+//# sourceMappingURL=index.js.map
